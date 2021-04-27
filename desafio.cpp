f67139beb720dc0463a039204ad46379672840e7
João <joao.vigoraes@gmail.com>
2021-04-27 03:44:17 -0300

    terceiro

--- desafio.cpp
+++ desafio.cpp
@@ -3,9 +3,76 @@
 #include <vector>
 using namespace std;
 
+//classe para o bd transações
+class Transactions{
+private:
+	int id_accountUser;
+	string option;
+	//struct e vetor que armazenam os dados
+	struct TRANSACTION{
+		int id, id_account;
+		double value;
+		//time_t t_hour;
+		string s_hour, type_transation;
+	};
+	vector<TRANSACTION> v;
+
+	//variaveis dos bd
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+	TRANSACTION t;
+
+
+public:
+	Transactions(){
+		rc = sqlite3_open("base.db", &db);		
+	}
+
+	void init(int id_accountUser, string option){
+		this->id_accountUser = id_accountUser;
+		this->option = option;
+		
+		dataRetrieve();
+		
+		if(option == "Extrato") printTransactions();
+	}
+
+	//função de armazenamento de dados do bd
+	static int callback_Transactions(void *data, int argc, char **argv, char **azColName){
+		vector<TRANSACTION>* v = static_cast<vector<TRANSACTION>*>(data);
+		
+		v->push_back({atoi(argv[0]), atoi(argv[4]), atof(argv[3]), argv[1], argv[2]});
+		return 0;
+	}
+
+	void dataRetrieve(){
+		//preparando o comando sql
+		string sql = "SELECT * from transações\nWHERE id_conta = " + to_string(id_accountUser) + ";";
+		//executando o comando e armazenando os dados na struct
+		rc = sqlite3_exec(db, sql.c_str(), callback_Transactions, &v, &zErrMsg);
+	}
+
+	void close(){
+		sqlite3_close(db);
+	}
+
+	//printa o extrato
+	void printTransactions(){
+		cout << "\t# EXTRATO #\n";
+		for(size_t i = 0; i < v.size(); i++){
+			cout << v.at(i).s_hour << " " << v.at(i).type_transation << " R$ " << v.at(i).value << endl; 
+		}
+		cout << "\n";
+	}
+};
+
+//class para o bd accounts
 class Users{
 
 private:
+	string check_account, check_password;
+	
 	//struct que armazena os dados do bd
 	struct USER{
 		int id;
@@ -13,7 +80,6 @@ private:
 		string account_number, password, account_type;
 	};
 	
-	string check_account, check_password;
 	
 	//variaveis dos bd
 	sqlite3 *db;
@@ -22,17 +88,44 @@ private:
 	USER u;
 	//const char* data = "Callback function called";
 
-	class Transactions{
+	Transactions transaction;
 
-private:
-	int id, id_account;
-	float value;
-	time_t t_hour;
-	string s_hour;
 public:
+	//construtor
+	Users(){
+		rc = sqlite3_open("base.db", &db);		
+		login();
+		option();
+		sqlite3_close(db);
+	}
 
-};
-	
+	//enquanto o usuário nao oferecer uma entrada compatível, o programa solicita conta e senha
+	void login(){ 
+		while(true){
+			cout << "\nInsira sua Conta Corrente: ";
+			cin >> check_account;
+			cout << "Insira sua Senha: ";
+			cin >> check_password;
+			dataRetrieve();
+			
+			if(u.account_number != "" && u.password != ""){
+					cout << "\nLogin realizado\n";
+					break;
+			}else{
+				cout << "\nLogin incorreto\n";		 
+			}
+		}
+		cout << "\n"; 
+	}
+
+	void dataRetrieve(){
+		//preparando o comando sql
+		string sql = "SELECT * from accounts\nWHERE conta_corrente = " + check_account + " AND senha = " + check_password + ";";
+		//executando o comando e armazenando os dados na struct
+		rc = sqlite3_exec(db, sql.c_str(), callback_User, &u, &zErrMsg);
+	}
+
+	//função de armazenamento de dados do bd
 	static int callback_User(void *data, int argc, char **argv, char **azColName){
 		USER *u = (USER *)data;
 		u->id = atoi(argv[0]);
@@ -43,71 +136,44 @@ public:
 		//cout << "Id = " << u->id << " account_number = " << u->account_number << " password = " << u->password << " account_type = " << u->account_type << " balance = " << u->balance << endl; 
 		return 0;
 	}
-public:
-	//construtor
-	Users(){
-		rc = sqlite3_open("base.db", &db);		
-		//enquanto o usuário não fornecer uma entrada compativel, o programa solicitará nova conta e senha
-		while(init());
-		option();
-		sqlite3_close(db);
-	}
-
-	bool init(){ 
-		cout << "\nInsira sua Conta Corrente: ";
-		cin >> check_account;
-		cout << "Insira sua Senha: ";
-		cin >> check_password;
-		dataRetrieve();
-		
-		if(u.account_number != "" && u.password != ""){
-				cout << "\nLogin realizado\n";
-				return false;
-		}else{
-			cout << "\nLogin incorreto\n";
-			return true;		 
-		}
-		// return true; 
-	}
-
-	void dataRetrieve(){
-		//preparando o comando sql
-		string sql = "SELECT * from accounts\nWHERE conta_corrente = " + check_account + " AND senha = " + check_password + ";";
-		//executando o comando e armazenando os dados na struct
-		rc = sqlite3_exec(db, sql.c_str(), callback_User, &u, &zErrMsg);
-	}
-
+	
 	void option(){
 		int choice; 
 		while(true){
 			do{
-				cout << "\nDigite o número correspondente a opção desejada:\n1. Ver Saldo\n2. Extrato\n3. Saque\n4. Depósito\n5. Transferencia\n6. Solicitar visita do gerente \n7. Trocar de usuário\n8. Sair\n\n";
+				cout << "Digite o número correspondente a opção desejada:\n\n1. Ver Saldo\n2. Extrato\n3. Saque\n4. Depósito\n5. Transferencia\n6. Solicitar visita do gerente \n7. Trocar de usuário\n8. Sair\n\n";
 				cin >> choice;
+				cout << "\n";
 			}while(choice < 0 || choice > 9);
 			
 			if(choice == 8) break;
-			
+			else if(choice == 7){
+				Users user;
+				break;	
+			}
 			switch(choice){
 				case 1:
-					cout << "\nSeu saldo é de R$ " << u.balance << endl << endl;
+					cout << "Seu saldo é de R$ " << u.balance << endl << endl;
 					break;
 				case 2:
+					transaction.init(u.id, "Extrato");
 					break;
 				case 3:
+					transaction.init(u.id, "Saque");	
 					break;
 				case 4:
+					transaction.init(u.id, "Depósito");
 					break;
 				case 5:
+					transaction.init(u.id, "Transferência");
 					break;	
 				case 6:
 					break;
-				case 7:
-					break;
 			}
 		}
+		transaction.close();
 	}
 };
-
 
 
 int main(int argc, char const *argv[]){
